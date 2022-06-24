b29f83f3dc53d15a57785dbd81f77501d143f0e3
Matthew Larkin <matthewlarkin8@gmail.com>
2022-06-23 23:09:02 -0400

    Refactored code by creating new nested component.

--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import WeatherDay from './WeatherDay';
 import Table from 'react-bootstrap/Table'
 import './Weather.css';
 
+
 class Weather extends React.Component{
   render(){
+    let weatherDay=this.props.weatherData.map((day,idx)=>{
+      return<WeatherDay
+      key={idx}
+      datetime={day.datetime}
+      description={day.description}
+      ></WeatherDay>
+    })
+
     return(
-      <Table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Weather</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weatherData.map((day,idx)=>(
-            <tr key={idx}>
-              <td>{day.datetime}</td>
-              <td>{day.description}</td>
+      <>
+        <h2>Weather</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Weather</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {weatherDay}
+          </tbody>
+        </Table>
+      </>
     )
   };
 };
